dc99bae4ca48762e96678c44b582896ad9a56f1c
tungit2507 <131683573+tungit2507@users.noreply.github.com>
2023-11-14 21:37:37 +0700

    set up back-end

---
+++ zing/src/main/java/com/m2m/zing/ZingApplication.java
@@ -0,0 +1,13 @@
+package com.m2m.zing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZingApplication.class, args);
+	}
+
+}
---
+++ zing/src/test/java/com/m2m/zing/ZingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2m.zing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
