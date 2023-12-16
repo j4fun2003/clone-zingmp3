64d68e2fa7eec31ca0f37f9b930786b8bd09f3d4
tungit2507 <131683573+tungit2507@users.noreply.github.com>
2023-12-16 16:58:09 +0700

    gg drive api

---
+++ zing/src/main/resources/static/assets/javascript/api/testAPI.js
@@ -0,0 +1,8 @@
+// function upload() {
+//     let file = document.getElementById("song-music").files[0];
+//     if(file){
+//         uploadFileToDrive(file);
+//     }else{
+//         console.log("failed");
+//     }
+// }
