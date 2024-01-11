b06d3b47c05c8cf32ee9463e9b2e9306dc492dd6
Thai Huy <j4fun2003@gmail.com>
2024-01-11 20:52:52 +0700

    QuocDat - Commmit -v2

--- zing/src/main/java/com/m2m/zing/controller/TestUser.java
+++
@@ -1,9 +0,0 @@
-package com.m2m.zing.controller;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class TestUser {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123"));
-    }
-}
